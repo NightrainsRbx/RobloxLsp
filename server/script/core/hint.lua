39e2c9f676a67af53c7ad5547096e9b93d7a89f3
NightrainsRbx <andres.parcero@outlook.com>
2021-09-02 21:25:40 -0500

    inlay hint show nil type

--- server/script/core/hint.lua
+++ server/script/core/hint.lua
@@ -42,9 +42,7 @@ local function typeHint(uri, edits, start, finish)
         end
         local infer = vm.getInferType(source, 0)
 
-        if  not source.value
-        and infer == 'any'
-        or  infer == 'nil' then
+        if not source.value and (infer == 'any' or infer == 'nil') then
             return
         end
         if #infer > 30 then
