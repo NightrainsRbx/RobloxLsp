fa39a1024f3b746c69319f6c2d4251e785b977f2
NightrainsRbx <andres.parcero@outlook.com>
2022-03-03 18:30:03 -0400

    1.5.6

--- client/out/extension.js
+++ client/out/extension.js
@@ -77,7 +77,7 @@ function updateRobloxAPI(context) {
 }
 function openUpdatesWindow(context) {
     return __awaiter(this, void 0, void 0, function* () {
-        if (context.globalState.get("sawVersionLogNew11", false) == false) {
+        if (context.globalState.get("sawVersionLogNew12", false) == false) {
             const panel = vscode.window.createWebviewPanel('robloxlspUpdates', 'Roblox LSP Updates', vscode.ViewColumn.One, {});
             panel.webview.html = `<!DOCTYPE html>
         <html lang="en">
@@ -92,75 +92,15 @@ function openUpdatesWindow(context) {
                 <p style="font-size:1rem">More info: <a href="https://devforum.roblox.com/t/roblox-lsp-full-intellisense-for-roblox-and-luau/717745">https://devforum.roblox.com/t/roblox-lsp-full-intellisense-for-roblox-and-luau/717745</a></p>
                 <p style="font-size:1rem">Report any bug or question here: <a href="https://github.com/NightrainsRbx/RobloxLsp/issues">https://github.com/NightrainsRbx/RobloxLsp/issues</a></p>
                 <hr style="height:2px;border:none;color:#333;background-color:#333;"/>
-                <h2 style="font-size:2rem; font-weight:100">1.5.3</h2>
-                <li style="font-size:1rem">Added better documentation for Roblox API.</li>
-                <li style="font-size:1rem">Added support for .luau file extensions.</li>
-                <li style="font-size:1rem">Vector3.FromNormalId/FromAxis is no longer deprecated.</li>
-                <li style="font-size:1rem">Improved type inference and fixed some bugs.</li>
-                <h2 style="font-size:2rem; font-weight:100">1.5.1</h2>
-                <li style="font-size:1rem">Improved type inference.</li>
-                <li style="font-size:1rem">Fixed some bugs.</li>
-                <h2 style="font-size:2rem; font-weight:100">1.5.0</h2>
-                <li style="font-size:1rem">Support for if expressions.</li>
-                <li style="font-size:1rem">Added table.freeze and table.isfrozen.</li>
-                <h2 style="font-size:2rem; font-weight:100">1.4.0</h2>
-                <li style="font-size:1rem">Support for Rojo project composition and Wally.</li>
-                <li style="font-size:1rem">Added robloxLsp.intelliSense.autoDetectLibraries.</li>
-                <li style="font-size:1rem">Improved markdown in descriptions.</li>
-                <h2 style="font-size:2rem; font-weight:100">As of 1.3.1</h2>
-                <li style="font-size:1rem">Added progress notification when updating API.</li>
-                <li style="font-size:1rem">Added Color3.fromHex (as deprecated) with Color preview.</li>
-                <li style="font-size:1rem">$className is no longer required in services for Rojo project files.</li>
-                <li style="font-size:1rem">Added syntax support for generic functions.</li>
-                <li style="font-size:1rem">Ignored files are now compiled for IntelliSense when requiring them.</li>
-                <li style="font-size:1rem">Added robloxLsp.workspace.loadRequiredFiles.</li>
-                <li style="font-size:1rem">A lot of fixes.</li>
-                <h2 style="font-size:2rem; font-weight:100">1.3.0</h2>
-                <li style="font-size:1rem">More improvements to type inference.</li>
-                <li style="font-size:1rem">Improved how typeof (the type) works.</li>
-                <li style="font-size:1rem">Implemented type inference and type checking of metatables.</li>
-                <li style="font-size:1rem">Added utility type WithMeta&lt;T, M&gt; (may change).</li>
-                <li style="font-size:1rem">Improved preloading of workspace.</li>
-                <li style="font-size:1rem">Fixed many bugs.</li>
-                <h2 style="font-size:2rem; font-weight:100">1.2.2</h2>
-                <li style="font-size:1rem">Improved type inference by a lot.</li>
-                <li style="font-size:1rem">Fixed many bugs with auto-completion.</li>
-                <li style="font-size:1rem">Methods now only show up in autocomplete if indexed with ":".</li>
-                <li style="font-size:1rem">Fixed Get/Find All References.</li>
-                <h2 style="font-size:2rem; font-weight:100">1.2.0</h2>
-                <li style="font-size:1rem">Improved support for Rojo, is faster, and doesn't depend on your project being a child of DataModel.</li>
-                <li style="font-size:1rem">Type Intersections are now type-checked when inferring their return type.</li>
-                <li style="font-size:1rem">Added snippets for callbacks like in RBXScriptSignals.</li>
-                <li style="font-size:1rem">Improved auto-completion of fields inside tables with table types.</li>
-                <li style="font-size:1rem">Diagnostics for type names and type aliases are now independent of type checking.</li>
-                <li style="font-size:1rem">Improved type inference of binary and unary operations.</li>
-                <li style="font-size:1rem">Improved syntax highlighting for "not", "and", and "or" keywords.</li>
-                <h2 style="font-size:2rem; font-weight:100">1.1.0</h2>
-                <li style="font-size:1rem">Added support for Promise, Rodux and Roact-Rodux.</li>
-                <li style="font-size:1rem">Improved type inference based on cursor position and type asserts.</li>
-                <li style="font-size:1rem">Added setting "robloxLsp.typeChecking.showFullType".</li>
-                <li style="font-size:1rem">Fixed some bugs.</li>
-                <h2 style="font-size:2rem; font-weight:100">1.0.0</h2>
-                <li style="font-size:1rem">Roblox LSP got updated to Lua by sumneko 1.21.3! This version has new features like inlay hints, plugins, external libraries and more.</li>
-                <li style="font-size:1rem">Much faster, stable and intelligent.</li>
-                <li style="font-size:1rem">All features have been reworked and improved.</li>
-                <li style="font-size:1rem">Pretty much everything has changed so I will not specify the details here, I'll document the most important features on the github wiki page soon.</li>
-                <h2 style="font-size:2rem; font-weight:100">Luau Intellisense and Typechecking</h2>
-                <p style="font-size:1rem">Roblox LSP is now able to understand Luau type annotations, to provide full intellisense, and even type checking!</p>
-                <p style="font-size:1rem">Typechecking is in BETA right now, and disabled by default, don't use it in serious projects yet. Features that need to be implemeted:</p>
-                <ul>
-                    <li>Type Refinements and custom Type Predicates.</li>
-                    <li>Support for Metatables, at the moment, setting a metatable will result in a union of the __index field of the metatable and the table.</li>
-                    <li>Compatibility check for Type Intersection.</li>
-                    <li>Generic for Functions (to be used internally like in Roblox Studio).</li>
-                    <li>Much better type inference.</li>
-                </ul>
-                <p style="font-size:1rem">If you want to test typechecking, set "robloxLsp.typeChecking.mode" to "Strict" or "Non Strict". Intellisense is enabled by default.</p>
-                <p style="font-size:1rem">Type checking in Roblox LSP is heavily based on the official Luau type checking, but it's not completely the same, and it's not intended to be. For more details, check: <a href="https://github.com/NightrainsRbx/RobloxLsp/wiki/Type-Checking">https://github.com/NightrainsRbx/RobloxLsp/wiki/Type-Checking</a></p>
-                <h2 style="font-size:2rem; font-weight:100">Focusing only on Roblox</h2>
-                <p style="font-size:1rem">The namespace for the settings in Roblox LSP is now "robloxLsp" instead of "Lua", also, from now Roblox LSP doesn't support other versions of Lua like Lua 5.1 to LuaJIT, to work with these versions, install Lua by sumneko separately.</p>
-                <p style="font-size:1rem">I recommend not disabling the settings you used to have disabled in the previous version, since they are much better now, you should give them a second chance.</p>
-                <p style="font-size:1rem">The setting "Lua.diagnostics.syntaxErrors" has been removed, to use Selene, disable diagnostics completely, this will also disable syntax errors. Type checking will not be affected by this.</p>
+                <h2 style="font-size:2rem; font-weight:100">v1.5.6</h2>
+                <li style="font-size:1rem">Improved syntax support for Luau and added basic support for type packs.</li>
+                <li style="font-size:1rem">Implemented suggeted imports for modules (thanks to <a href="https://github.com/Corecii">@Corecii</a>, <a href="https://github.com/NightrainsRbx/RobloxLsp/pull/123">#123</a>)</li>
+                <li style="font-size:1rem">Added Vector3 constants (thanks to <a href="https://github.com/aku-e">@aku-e</a>, <a href="https://github.com/NightrainsRbx/RobloxLsp/pull/145">#145</a>)</li>
+                <li style="font-size:1rem">Color3.fromHex is no longer deprecated.</li>
+                <li style="font-size:1rem">Fixed deprecated classes not having typings. (<a href="https://github.com/NightrainsRbx/RobloxLsp/issues/147">#147</a>)</li>
+                <li style="font-size:1rem">Fixed embedded rojo projects not using their correct root name. (<a href="https://github.com/NightrainsRbx/RobloxLsp/issues/128">#128</a>)</li>
+                <li style="font-size:1rem">Inlay hints are not displayed if the type is "none" or "any".</li>
+                <li style="font-size:1rem">Updated binaries.</li>
             </div>
         </body>
         </html>`;
